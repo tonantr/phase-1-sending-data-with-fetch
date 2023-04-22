0504d11cbe73ca8d4aa4c298a0de25e144358026
tonantr <tonantruong@hotmail.com>
2023-04-22 19:33:39 -0400

    complete Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(params1, params2) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${params1}`,
+            email: `${params2}`
+        })
+    })
+        .then(res => { return res.json() })
+        .then((data) => { renderUsers(data) })
+        .catch((error) => {
+            document.querySelector('body').innerHTML = error;
+        })
+};
+
+function renderUsers(data) {
+    const bodyTag = document.querySelector('body');
+    const pTag = document.createElement('p');
+    pTag.innerHTML = data.id;
+    bodyTag.appendChild(pTag);
+};
+
+
+
+
